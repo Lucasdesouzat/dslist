25617b99ed27197556b561bb5afc712f66d2de8f
lucasdesouzat <lucasdesouzat@gmail.com>
2023-05-21 18:58:28 -0300

    GET lists

---
+++ src/main/java/com/tamarin/dslist/dto/GameListDTO.java
@@ -0,0 +1,26 @@
+package com.tamarin.dslist.dto;
+
+import com.tamarin.dslist.entities.GameList;
+
+public class GameListDTO {
+
+	private Long id;
+	private String name;
+
+	public GameListDTO() {
+	}
+
+	public GameListDTO(GameList entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+		
+}
